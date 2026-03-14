60bb925faced50473fe6992c88ecb25eefc96439
Ibrahim54-glitched <ibnshahbaz244@gmail.com>
2026-03-14 20:33:10 +0500

    first commit

---
+++ .zig-cache/o/08e33aa10a12745a90a76ea7362490a7/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"zigimg-0.1.0-8_eo2qWHFwA-S24SH7x7NplPii0EbIQiUfDrimgA6rzN" = struct {
+        pub const build_root = "C:\\Users\\Ibrahim\\AppData\\Local\\zig\\p\\zigimg-0.1.0-8_eo2qWHFwA-S24SH7x7NplPii0EbIQiUfDrimgA6rzN";
+        pub const build_zig = @import("zigimg-0.1.0-8_eo2qWHFwA-S24SH7x7NplPii0EbIQiUfDrimgA6rzN");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "zigimg", "zigimg-0.1.0-8_eo2qWHFwA-S24SH7x7NplPii0EbIQiUfDrimgA6rzN" },
+};
---
+++ src/main.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,\\\"^`\". ";
+    const gscale2 = "@%#*+=-:. ";
+
+    _ = gscale1;
+    _ = gscale2;
+}
